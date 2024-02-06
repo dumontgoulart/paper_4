42e54d9b975c43394a1206ec92ccf7df83acac2c
Henrique Goulart <45360568+dumontgoulart@users.noreply.github.com>
2024-02-06 16:02:25 +0100

    Update scenario and resolution in sfincs_scenarios_setup.py

--- sfincs_scenarios_setup.py
+++ sfincs_scenarios_setup.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         "epsg": 32736,}
 
     # general parameters ###
-    res = 5
+    res = 20
     storm = 'idai'
     sim_name = 'ifs_rebuild_bc'
 
@@ -84,13 +84,16 @@ if __name__ == "__main__":
         "dz": 2,
     }
 
-    # libraries
-    data_libs = ['d:/paper_4/data/data_catalogs/data_catalog_converter.yml', root_folder+f'data_deltares_{storm}/data_catalog.yml']
-    list_indices_storm = ['merit_hydro','gebco', 'osm_coastlines', 'osm_landareas', 'gswo', 'fabdem', 'dtu10mdt', 'gcn250', 'vito', "rivers_lin2019_v1"]
+    # data and libraries
+    data_libs = ['d:/paper_4/data/data_catalogs/data_catalog_converter.yml',
+                 root_folder+f'data_deltares_{storm}/data_catalog.yml']
+    
+    list_indices_storm = ['merit_hydro','gebco', 'osm_coastlines', 'osm_landareas', 
+                          'gswo', 'fabdem', 'dtu10mdt', 'gcn250', 'vito', "rivers_lin2019_v1"]
 
     ##########################################################################################
     # optional step: create data catalog
-    # sfincs_scen.clip_data_to_region(bbox_beira, export_dir = f'data_deltares_test', data = ['deltares_data', data_libs[0]], list_indices = list_indices_storm)
+    # sfincs_scen.clip_data_to_region(bbox_beira, export_dir = f'data_deltares_idai', data = ['deltares_data', data_libs[0]], list_indices = list_indices_storm)
 
     # 1) Offshore model
     # historical
@@ -121,50 +124,53 @@ if __name__ == "__main__":
                             bbox = bbox_beira, topo_map = 'beira_dem', res = res,
                             tref = tref_hightide, tstart = tstart_hightide, tstop = tstop_hightide)  
 
-# Define physical and adaptation scenarios
-physical_scenarios = {
-    "hist_rain_surge": {
-        "base_root_suffix": "base",
-        "scenario_root_suffix": f"{sim_name}_hist_rain_surge_noadapt",
-        "precip_path_suffix": "hourly",
-        "waterlevel_path_folder": ""
-    },
-    "3c_rain_surge": {
-        "base_root_suffix": "base",
-        "scenario_root_suffix": f"{sim_name}_3c_rain_surge_noadapt",
-        "precip_path_suffix": "tp_3c_hourly",
-        "waterlevel_path_folder": "",
-        "slr": slr_level
-    },
-    "hightide_rain_surge": {
-        "base_root_suffix": "base_hightide",
-        "scenario_root_suffix": f"{sim_name}_hightide_rain_surge_noadapt",
-        "precip_path_suffix": "hightide_hourly",
-        "waterlevel_path_folder": "_hightide"
-    },
-    "3c-hightide_rain_surge": {
-        "base_root_suffix": "base_hightide",
-        "scenario_root_suffix": f"{sim_name}_3c-hightide_rain_surge_noadapt",
-        "precip_path_suffix": "tp_3c-hightide_hourly",
-        "waterlevel_path_folder": "_hightide",
-        "slr": slr_level
+    # Define physical and adaptation scenarios
+    physical_scenarios = {
+        "hist_rain_surge": {
+            "base_root_suffix": "base",
+            "scenario_root_suffix": f"{sim_name}_hist_rain_surge_noadapt",
+            "precip_path_suffix": "hourly",
+            "waterlevel_path_folder": ""
+        },
+        "3c_rain_surge": {
+            "base_root_suffix": "base",
+            "scenario_root_suffix": f"{sim_name}_3c_rain_surge_noadapt",
+            "precip_path_suffix": "tp_3c_hourly",
+            "waterlevel_path_folder": "",
+            "slr": slr_level
+        },
+        "hightide_rain_surge": {
+            "base_root_suffix": "base_hightide",
+            "scenario_root_suffix": f"{sim_name}_hightide_rain_surge_noadapt",
+            "precip_path_suffix": "hightide_hourly",
+            "waterlevel_path_folder": "_hightide"
+        },
+        "3c-hightide_rain_surge": {
+            "base_root_suffix": "base_hightide",
+            "scenario_root_suffix": f"{sim_name}_3c-hightide_rain_surge_noadapt",
+            "precip_path_suffix": "tp_3c-hightide_hourly",
+            "waterlevel_path_folder": "_hightide",
+            "slr": slr_level
+        }
     }
-}
 
-for scenario_name, scenario_config in physical_scenarios.items():
-    scenario_root=f'{root_folder}{storm}_{scenario_config["scenario_root_suffix"]}'
-    print(f'Creating scenario: {scenario_name}')
-    sfincs_scen.update_sfincs_model(base_root=f'{root_folder}{storm}_{scenario_config["base_root_suffix"]}',
-                                    new_root = scenario_root, data_libs=data_libs, mode='rain_surge',
-                                    precip_path=f'{sim_name}_{scenario_config["precip_path_suffix"]}',
-                                    waterlevel_path=rf'D:/paper_4/data/sfincs_input/quadtree_{sim_name}{scenario_config["waterlevel_path_folder"]}/sfincs_his.nc',
-                                    slr=scenario_config.get("slr"))
-    
-    for measure in ['hold', 'retreat']:
-            adapt_root = scenario_root.replace("_noadapt", f"_{measure}")
-            adapt_params = hold_params if measure == 'hold' else retreat_params
-            # add adaptation measures
-            sfincs_scen.add_adaptation_measures(data_libs=data_libs, 
-                                                original_root=scenario_root,
-                                                new_root=adapt_root, structures_params=adapt_params)
+    for scenario_name, scenario_config in physical_scenarios.items():
+        scenario_root=f'{root_folder}{storm}_{scenario_config["scenario_root_suffix"]}'
+        print(f'Creating scenario: {scenario_name}')
+        sfincs_scen.update_sfincs_model(base_root=f'{root_folder}{storm}_{scenario_config["base_root_suffix"]}',
+                                        new_root = scenario_root, data_libs=data_libs, mode='rain_surge',
+                                        precip_path=f'{sim_name}_{scenario_config["precip_path_suffix"]}',
+                                        waterlevel_path=rf'{root_folder}quadtree_{sim_name}{scenario_config["waterlevel_path_folder"]}/sfincs_his.nc',
+                                        slr=scenario_config.get("slr"))
+        
+        for measure in ['hold', 'retreat']:
+                adapt_root = scenario_root.replace("_noadapt", f"_{measure}")
+                adapt_params = hold_params if measure == 'hold' else retreat_params
+                # add adaptation measures
+                sfincs_scen.add_adaptation_measures(data_libs=data_libs, 
+                                                    original_root=scenario_root,
+                                                    new_root=adapt_root, structures_params=adapt_params)
 
+
+    # optional: run locally sfincs model
+    # sfincs_scen.run_sfincs(base_root = rf'D:\paper_4\data\sfincs_input\{storm}_{sim_name}_3c-hightide_rain_surge_noadapt', fn_exe = fn_exe)
